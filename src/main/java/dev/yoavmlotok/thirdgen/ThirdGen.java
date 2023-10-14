406436bd55a7c64676b54dc21459e23afc487d3e
Yoav Mlotok <yoavmlotok@icloud.com>
2023-10-14 12:35:06 +0300

    transformed template into the thirdgen mod

---
+++ src/main/java/dev/yoavmlotok/thirdgen/ThirdGen.java
@@ -0,0 +1,11 @@
+package dev.yoavmlotok.thirdgen;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class ThirdGen implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
