aab1293b672e403825d69bfdf3d08adca7108d44
Yoav Mlotok <yoavmlotok@icloud.com>
2023-10-17 22:04:02 +0300

    finalized field

--- src/main/java/dev/yoavmlotok/thirdgen/ThirdGen.java
+++ src/main/java/dev/yoavmlotok/thirdgen/ThirdGen.java
@@ -6,7 +6,7 @@ import net.minecraft.client.option.KeyBind;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 public class ThirdGen implements ClientModInitializer {
-	public static KeyBind zoomKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBind("key.thirdgen.zoom",InputUtil.KEY_C_CODE, "key.categories.thirdgen.thirdgen"));
+	public static final KeyBind zoomKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBind("key.thirdgen.zoom",InputUtil.KEY_C_CODE, "key.categories.thirdgen.thirdgen"));
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
